1978041dd33f55d4d10bc327729a7a1d2988fbaf
tomoka <tomoka12turquoise@gmail.com>
2025-03-19 10:30:16 +0900

    step5 items.sqlの更新

--- go/db/items.sql
+++ go/db/items.sql
@@ -2,5 +2,11 @@ CREATE TABLE items (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     category_id INTEGER NOT NULL,
-    image_name TEXT NOT NULL
+    image_name TEXT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
 );
